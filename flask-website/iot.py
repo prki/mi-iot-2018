6b6fc166371234930edfe025484921ed7778a05a
prk <piknamar@fit.cvut.cz>
2018-04-26 20:52:28 +0200

    Basic graph displaying and folder layout

--- flask-website/iot.py
+++ flask-website/iot.py
@@ -1,10 +1,36 @@
 from flask import Flask, g, render_template
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 
+"""
+Function generating the graph to be displayed on the website.
+
+Currently draws a simple plot taken from matplotlib tutorial
+https://matplotlib.org/gallery/lines_bars_and_markers/simple_plot.html
+"""
+def generateGraph():
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+            title='About as simple as it gets, folks')
+    ax.grid()
+
+    fig.savefig('static/graph.png')
+
+
 @app.route('/')
 def main_page():
-    return '<3'
+    generateGraph()
+
+    return render_template('main_page.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
